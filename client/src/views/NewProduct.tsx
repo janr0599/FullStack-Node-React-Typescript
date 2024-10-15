085be023056040abffbda9b14146e864220234c2
janr0599 <javiernr0599@gmail.com>
2024-10-15 10:38:13 -0400

    Final changes

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,6 +1,33 @@
-import { Link } from "react-router-dom";
+import {
+    Link,
+    Form,
+    useActionData,
+    ActionFunctionArgs,
+    redirect,
+} from "react-router-dom";
+import ErrorMessage from "../components/ErrorMessage";
+import { addProduct } from "../services/ProductService";
+import ProductForm from "../components/ProductForm";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const data = Object.fromEntries(await request.formData());
+
+    let error = "";
+    if (Object.values(data).includes("")) {
+        error = "All fields are required";
+    }
+    if (error.length) {
+        return error;
+    }
+
+    await addProduct(data);
+
+    return redirect("/");
+};
 
 function NewProduct() {
+    const error = useActionData() as string;
+
     return (
         <>
             <div className="flex justify-between">
@@ -14,6 +41,15 @@ function NewProduct() {
                     Back to products
                 </Link>
             </div>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            <Form className="mt-10" method="POST">
+                <ProductForm />
+                <input
+                    type="submit"
+                    className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+                    value="Add Product"
+                />
+            </Form>
         </>
     );
 }
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,6 +1,24 @@
-import { Link } from "react-router-dom";
+import { ActionFunctionArgs, Link, useLoaderData } from "react-router-dom";
+import {
+    getProducts,
+    updateProductavailability,
+} from "../services/ProductService";
+import ProductDetails from "../components/ProductDetails";
+import { Product } from "../types";
+
+export const loader = async () => {
+    const products = await getProducts();
+    return products;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const data = Object.fromEntries(await request.formData());
+    await updateProductavailability(+data.id);
+    return {};
+};
 
 function Products() {
+    const products = useLoaderData() as Product[];
     return (
         <>
             <div className="flex justify-between">
@@ -11,6 +29,27 @@ function Products() {
                 >
                     Add new product
                 </Link>
+            </div>
+
+            <div className="p-2">
+                <table className="w-full mt-5 table-auto">
+                    <thead className="bg-slate-800 text-white">
+                        <tr>
+                            <th className="p-2">Product</th>
+                            <th className="p-2">Price</th>
+                            <th className="p-2">Availability</th>
+                            <th className="p-2">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product) => (
+                            <ProductDetails
+                                key={product.id}
+                                product={product}
+                            />
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     );
